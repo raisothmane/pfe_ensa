489a8b3a2185c1738d5031c32aca3a8dd91b6727
raisothmane <RAIS.OTHMANE@ofppt-edu.ma>
2024-06-15 23:38:58 +0100

    chore: Update navigation.blade.php to remove unnecessary code and improve responsiveness

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -95,12 +95,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
        
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-            
-        </div>
+       
         <div class="pt-2 pb-3 space-y-1">
         <x-nav-link :href="route('user_table')" :active="request()->routeIs('user_table')">
                     {{ __('User Table') }}
