41d38bc23f3db5cbef0798dc2e2c53643ef09dad
raisothmane <RAIS.OTHMANE@ofppt-edu.ma>
2024-07-06 21:19:32 +0100

    chore: Update page title in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Quiz Game') }}</title>
+        <title>Quiz Game</title>
         <link rel="icon" type="image/x-icon" href="{{ asset('img/logo1.png') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
